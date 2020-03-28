78cc27ac0f2ec68e66836e268f4f445e04270505
NabeelSeid <nabeelseid@gmail.com>
2020-03-28 23:13:20 +0300

    Internal Component State

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -21,23 +21,25 @@ const list = [
   }
 ];
 
-function App() {
-  var helloWorld = "Welcome to the road to lear react";
+class App extends Component {
+  state = { list };
 
-  return (
-    <div className="App">
-      {list.map(item => (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </div>
-      ))}
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        {this.state.list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
