f5983aced6d7f58004f84a07f536680a24b90d5b
NabeelSeid <nabeelseid@gmail.com>
2020-03-31 18:52:15 +0300

    Paginated Fetch

--- src/App.js
+++ src/App.js
@@ -1,66 +1,76 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, {Component} from 'react'
+import './App.css'
 
-const DEFAULT_QUERY = "redux";
+const DEFAULT_QUERY = 'redux'
+const DEFAULT_HPP = '100'
 
-const PATH_BASE = "https://hn.algolia.com/api/v1";
-const PATH_SEARCH = "/search";
-const PARAM_SEARCH = "query=";
+const PATH_BASE = 'https://hn.algolia.com/api/v1'
+const PATH_SEARCH = '/search'
+const PARAM_SEARCH = 'query='
+const PARAM_PAGE = 'page='
+const PARAM_HPP = 'hitPerPage='
 
 // const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = { result: null, searchTerm: DEFAULT_QUERY };
+    super(props)
+    this.state = {result: null, searchTerm: DEFAULT_QUERY}
 
-    this.setSearchTopStories = this.setSearchTopStories.bind(this);
-    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
-    this.onSearchSubmit = this.onSearchSubmit.bind(this);
-    this.onDismiss = this.onDismiss.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
+    this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    this.onDismiss = this.onDismiss.bind(this)
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const {hits, page} = result
+
+    const oldHits = page !== 0 ? this.state.result.hits : []
+
+    result = {...result, hits: [...oldHits, ...hits]}
+
+    this.setState({result})
   }
 
-  fetchSearchTopStories(searchTerm) {
-    const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`;
+  fetchSearchTopStories(searchTerm, page = 0, hpp = DEFAULT_HPP) {
+    const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&
+      ${PARAM_PAGE}${page}&${PARAM_HPP}${hpp}`
 
     fetch(url)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(err => err);
+      .catch(err => err)
   }
 
   componentDidMount() {
-    console.log("sdafsda");
-    const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    const {searchTerm} = this.state
+    this.fetchSearchTopStories(searchTerm)
   }
 
   onSearchSubmit(event) {
-    const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
-    event.preventDefault();
+    const {searchTerm} = this.state
+    this.fetchSearchTopStories(searchTerm)
+    event.preventDefault()
   }
 
   onSearchChange = event => {
-    this.setState({ searchTerm: event.target.value });
-  };
+    this.setState({searchTerm: event.target.value})
+  }
 
   // binding and constructor can be avoided by using arrow function
   onDismiss(id) {
     const updatedHits = this.state.result.hits.filter(
-      item => item.objectID !== id
-    );
+      item => item.objectID !== id,
+    )
     this.setState({
-      result: { ...this.state.result, hits: updatedHits }
-    });
+      result: {...this.state.result, hits: updatedHits},
+    })
   }
 
   render() {
-    const { result, searchTerm } = this.state;
+    const {result, searchTerm} = this.state
+    const page = (result && result.page) || 0
 
     return (
       <div className="App page">
@@ -68,36 +78,45 @@ class App extends Component {
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
-            onSubmit={this.onSearchSubmit}
-          >
+            onSubmit={this.onSearchSubmit}>
             {/* Components can also be passed to another Component via props */}
             Search
           </Search>
-          {result && <Table list={result.hits} onDismiss={this.onDismiss} />}
+          {result && (
+            <Table list={result.hits} onDismiss={this.onDismiss} />
+          )}
+          <div className="interactions">
+            <Button
+              onClick={() =>
+                this.fetchSearchTopStories(searchTerm, page + 1)
+              }>
+              More
+            </Button>
+          </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-const Search = ({ searchTerm, onChange, onSubmit, children }) => (
+const Search = ({searchTerm, onChange, onSubmit, children}) => (
   <form onSubmit={onSubmit}>
     <input type="text" value={searchTerm} onChange={onChange} />
     <button type="submit">{children}</button>
   </form>
-);
+)
 
 const largeColumn = {
-  width: "40%"
-};
+  width: '40%',
+}
 const midColumn = {
-  width: "30%"
-};
+  width: '30%',
+}
 const smallColumn = {
-  width: "10%"
-};
+  width: '10%',
+}
 
-const Table = ({ list, onDismiss }) => (
+const Table = ({list, onDismiss}) => (
   <div className="table">
     {list.map(item => (
       <div key={item.objectID} className="table-row">
@@ -110,20 +129,19 @@ const Table = ({ list, onDismiss }) => (
         <span style={smallColumn}>
           <Button
             onClick={() => onDismiss(item.objectID)}
-            className="button-inline"
-          >
+            className="button-inline">
             Dismiss
           </Button>
         </span>
       </div>
     ))}
   </div>
-);
+)
 
-const Button = ({ onClick, className = "", children }) => (
+const Button = ({onClick, className = '', children}) => (
   <button onClick={onClick} className={className} type="button">
     {children}
   </button>
-);
+)
 
-export default App;
+export default App
